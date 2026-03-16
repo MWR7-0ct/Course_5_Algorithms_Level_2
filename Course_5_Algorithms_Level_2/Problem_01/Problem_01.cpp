7bf09d27bad90e6f9ce8e13473413ee17c68eef6
Muslim Wazir <momosese20@gmail.com>
2026-03-16 18:49:03 +0300

    Final code of problem#1

--- Course_5_Algorithms_Level_2/Problem_01/Problem_01.cpp
+++ Course_5_Algorithms_Level_2/Problem_01/Problem_01.cpp
@@ -1,9 +1,53 @@
-
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
+void PrintTableHeader() 
 {
-    std::cout << "Hello World!\n";
+
+	cout << "\n\n\t\t\t\t" << "Multiplication table from 1 to 10 \n" << endl;
+	cout << "\t";
+	for (int i = 1; i <= 10; i++)
+	{
+
+		cout << i << "\t" ;
+
+	}
+	cout << endl;
+	cout  << "\n------------------------------------------------------------------------------------" << endl;
 }
 
+string ColumSperator(int i)
+{
+
+	if (i < 10) 
+		return "   |";
+	else  
+		return "  |";
+
+}
+void PrintMultiplicationTable() 
+{
+	PrintTableHeader();
+
+	for (int i = 1; i <= 10; i++)
+	{
+		cout <<" " << i << ColumSperator(i) << "\t";
+
+		for (int j=1;j<=10;j++) 
+		{
+			cout << i * j << "\t";
+		}
+		cout << "\n------------------------------------------------------------------------------------" << endl;
+	}
+
+
+
+}
+
+int main() {
+	
+	PrintMultiplicationTable();
+
+	return 0;
+}
